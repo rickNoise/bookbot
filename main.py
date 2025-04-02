d95ec58debf0ecae8adeefd747f79ad799af8a86
rickNoise <emberix@gmail.com>
2025-04-02 18:13:00 +1100

    finished CH3 through L2

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import sys
 from stats import count_words, get_character_counts, dict_sorter
 
-FILE_PATH = "books/frankenstein.txt"
+# ensure user has entered appropriate number of args from CLI
+if len(sys.argv) != 2:
+    print("Usage: Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+FILE_PATH = sys.argv[1]
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,8 +15,6 @@ def get_book_text(filepath):
 def main():
     character_count_dict = get_character_counts(get_book_text(FILE_PATH))
     sorted_dict_list = dict_sorter(character_count_dict)
-    # print(f"{count_words(get_book_text(FILE_PATH))} words found in the document")
-    # print(character_count_dict)
 
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {FILE_PATH}...")
